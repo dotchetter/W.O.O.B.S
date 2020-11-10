8bbb9b1850a3c3061162d5f407eada4e83f8dcd3
Thro1901 <Rothen_90@msn.com>
2020-11-10 09:58:01 +0100

    added deepsleep for 10 sec. (need to connect RST pin with pin 0 after uploading code).

---
+++ Tempsensor_waterproof/config.h
@@ -0,0 +1,29 @@
+// ÄNDRA FÖLJANDE UPPGIFTER TILL DET SOM DU VILL ANVÄNDA
+
+#define DEVICE_ID "W.O.O.B.S"
+#define TEMPERATURE_ALERT 25
+#define SIMULATED_DATA false
+#define INTERVAL 10000
+
+
+#define durationSleep  10             // secondes to deep-sleep
+
+static char *ssid = "AndroidAP";
+static char *pass = "52105210"; 
+
+
+// Pin layout configuration
+
+#define WATERSENSOR 2 
+
+
+// EEPROM address configuration
+#define EEPROM_SIZE 512
+
+// SSID and SSID password's length should < 32 bytes
+// http://serverfault.com/a/45509
+#define SSID_LEN 32
+#define PASS_LEN 32
+#define CONNECTION_STRING_LEN 256
+
+#define MESSAGE_MAX_LEN 256
---
+++ Tempsensor_waterproof/time_init.h
@@ -0,0 +1,21 @@
+void Time_init()
+{
+    time_t epochTime;
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    while (true)
+    {
+        epochTime = time(NULL);
+
+        if (epochTime == 28800)
+        {
+            Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
+            delay(2000);
+        }
+        else
+        {
+            Serial.printf("Fetched NTP epoch time is: %lu.\r\n", epochTime);
+            break;
+        }
+    }
+}
