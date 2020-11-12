92ad1d003001ddd4efe9689baacf60b7a56e368e
Simon Olofsson <dotchetter@protonmail.ch>
2020-11-12 10:14:20 +0100

    Added project files

---
+++ embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * ATMEL_SAMD21G18A_WOOBS_FW.cpp
+ *
+ * Created: 2020-11-11 18:39:12
+ * Author : Simon
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
