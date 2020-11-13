611580605ec4db79e27193bdd79983aebe82ee8a
Thomas Roth <rothen_90@msn.com>
2020-11-13 10:55:00 +0100

    Added defines for Ports and pin to DS18B20 Added defines for function for pin input and output

--- embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/main.cpp
+++ embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/main.cpp
@@ -33,3 +33,13 @@ int main(void)
 #define F_CPU 48000000UL //Your clock speed in Hz (3Mhz here)   ÄNDRA ANTAR JAG
 #define LOOP_CYCLES 8 //Number of cycles that the loop takes
 #define us(num) (num/(LOOP_CYCLES*(1/(F_CPU/1000000.0))))
+#define THERM_PORT PORT[0]
+//#define THERM_DDR DDRC
+#define THERM_PIN PORT_PA16  // (16ul)
+#define THERM_DQ PA16 // PIN D8 Mkr 1010 Wi-Fi
+/* Utils */
+#define THERM_INPUT_MODE() PORT->Group[0].PINCFG[16].reg = PORT_PINCFG_INEN    // PIN D8 as input
+#define THERM_OUTPUT_MODE() PORT->Group[0].DIRSET.reg = THERM_PIN // PIN D8 as output // kan vara DIR0 istället för DIRSET
+
+#define THERM_LOW() PORT->Group[0].OUTCLR.reg = THERM_PIN //PORT->Group[0].OUTCLR.reg     //THERM_PORT&=~(1<<THERM_DQ)
+#define THERM_HIGH() PORT->Group[0].OUTSET.reg = THERM_PIN //PORT->Group[0].OUTSET.reg     //THERM_PORT|=(1<<THERM_DQ)
