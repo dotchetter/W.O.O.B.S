905a2fefb4e89f4ee5a0b00433858085c92c17f4
Thomas Roth <rothen_90@msn.com>
2020-11-15 20:08:58 +0100

    Added 3-part lib for sleep. Sleeping for 8 sec

--- embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/ATMEL_SAMD21G18A_WOOBS_FW.ino
+++ embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/ATMEL_SAMD21G18A_WOOBS_FW.ino
@@ -1,44 +1,37 @@
-
-//#include "clock.h"
-#include "sam.h"
-#define F_CPU 48000000UL
-#include "delay.h"
-#include "definitions.h"
-#include "deepsleep.h"
+#include <ArduinoLowPower.h>
 
 
 
 void setup() {
     Serial.begin(9600);
-
-    REG_PORT_DIR0 = LED0_PIN_MASK;
-    
-    // Set LED0 OFF
-    REG_PORT_OUTCLR0 = LED0_PIN_MASK;
+    pinMode(9, OUTPUT);
   
-  
+    LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, dummy, CHANGE);
 }
 
 
 void loop() { 
 
+
+     Serial.begin(9600); 
+   
+   //  USBDevice.detach();
+
+    delay(10);
     Serial.println("vaknade precis loop");
-
-    // Set the drive strength to strong
-    PORT->Group[LED0_PORT].PINCFG[LED0_PIN_NUMBER].bit.DRVSTR = 1;
-        
-    // Turn the LED on PA17 ON
-    REG_PORT_OUTSET0 = LED0_PIN_MASK;
-        
-    // Errata: Make sure that the Flash does not power all the way down when in sleep mode. 
-    NVMCTRL->CTRLB.bit.SLEEPPRM = NVMCTRL_CTRLB_SLEEPPRM_DISABLED_Val;
+    Serial.flush();
+    delay(500);
+  digitalWrite(9, HIGH);
+  delay(500);
+  digitalWrite(9, LOW);
+  delay(500);
+            
+  LowPower.sleep(8000);
     
-    // Wait 5 seconds
-    delay_ms(5000);
-    
-    // Sleep
-    StandbySleep();
-    
-
+//USBDevice.attach();
+ delay(1000); 
  
+}
+void dummy() {
+  
 }
