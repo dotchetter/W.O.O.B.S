5ba0ddf1ef7ddc16154dfae83a844e1e235c6b64
Simon Olofsson <dotchetter@protonmail.ch>
2020-11-15 20:00:13 +0100

    Added files for DS18B20 driver.

---
+++ embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/DS18B20.h
@@ -0,0 +1,34 @@
+#include "include/port.h"
+
+/*
+
+    // Assign LED0 as OUTPUT
+    REG_PORT_DIR0 = LED0_PIN_MASK;
+    
+    // Set LED0 OFF
+    REG_PORT_OUTCLR0 = LED0_PIN_MASK;
+
+    // Turn the LED on PA17 ON
+    REG_PORT_OUTSET0 = LED0_PIN_MASK;
+
+*/
+
+class DS12B20
+{
+private:
+    uint8_t directionRegisterMask;
+    uint8_t microControllerPort;
+
+
+public:
+
+};
+
+
+
+
+/*
+
+    
+
+*/
