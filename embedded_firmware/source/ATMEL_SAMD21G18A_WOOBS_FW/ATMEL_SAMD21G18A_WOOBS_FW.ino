21911fff5afdc1e7ef3fd8a3da3aea7c09b4dde7
Thomas Roth <rothen_90@msn.com>
2020-11-15 16:49:49 +0100

    cleaned the code

--- embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/ATMEL_SAMD21G18A_WOOBS_FW.ino
+++ embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/ATMEL_SAMD21G18A_WOOBS_FW.ino
@@ -1,16 +1,28 @@
 
-#include "app.h"
 #include "clock.h"
 
 #define F_CPU 48000000UL
 #include "delay.h"
-
-#include "sleep.h"
+#include "definitions.h"
+#include "deepsleep.h"
 #include "sam.h"
 
 
 void setup() {
     Serial.begin(9600);
+
+    REG_PORT_DIR0 = LED0_PIN_MASK;
+    
+    // Set LED0 OFF
+    REG_PORT_OUTCLR0 = LED0_PIN_MASK;
+  
+  
+}
+
+
+void loop() { 
+
+    Serial.println("vaknade precis loop");
 
     // Set the drive strength to strong
     PORT->Group[LED0_PORT].PINCFG[LED0_PIN_NUMBER].bit.DRVSTR = 1;
@@ -27,14 +39,6 @@ void setup() {
     // Sleep
     StandbySleep();
     
-  
-}
-
-
-void loop() { 
-    Serial.begin(9600);
-    Serial.println("vaknade precis loop");
-    // Due to a hardware bug on the SAMD21, the SysTick interrupts become active before the flash has powered up from sleep, causing a hard fault
 
  
 }
