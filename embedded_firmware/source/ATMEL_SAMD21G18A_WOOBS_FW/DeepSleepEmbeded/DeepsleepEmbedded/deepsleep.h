6e7ffc22db6b31f99c0eaedb82febe23aafb0170
Thomas Roth <rothen_90@msn.com>
2020-11-16 16:04:26 +0100

    Added function for deepsleepInit Added function for deepsleep

--- embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/DeepSleepEmbeded/DeepsleepEmbedded/deepsleep.h
+++ embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/DeepSleepEmbeded/DeepsleepEmbedded/deepsleep.h
@@ -1,6 +1,10 @@
- void deepsleep(){
+void deepsleepInit(){
+  // Enable Deep Sleep Mode--------------------------------------------------------------
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;// | SCB_SCR_SLEEPONEXIT_Msk;  // Put the SAMD21 in deep sleep upon executing the __WFI() function
+  NVMCTRL->CTRLB.reg |= NVMCTRL_CTRLB_SLEEPPRM_DISABLED;        // Disable auto power reduction during sleep - SAMD21 Errata 1.14.2
+  }
+void deepsleep(){
   SysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk;           // Disable SysTick interrupts
-  __DSB();                                              // Complete outstanding memory operations - not required for SAMD21 ARM Cortex M0+
   __WFI();                                              // Put the SAMD21 into deep sleep, Zzzzzzzz... 
   SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;            // Enable SysTick interrupts
   }
