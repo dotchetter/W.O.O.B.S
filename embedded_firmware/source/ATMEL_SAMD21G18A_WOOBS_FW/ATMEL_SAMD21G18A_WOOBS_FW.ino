149320aae31ca108c3797bb7a4f168a351ea71ef
Thomas Roth <rothen_90@msn.com>
2020-11-17 16:36:01 +0100

    Copied code to test the wifi to main sketch

--- embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/ATMEL_SAMD21G18A_WOOBS_FW.ino
+++ embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/ATMEL_SAMD21G18A_WOOBS_FW.ino
@@ -1,37 +1,55 @@
-#include <ArduinoLowPower.h>
+#include <ArduinoJson.h>
+#include <ArduinoHttpClient.h>
+#include "config.h"
+#include "wifi.h"
 
+char server[] = ""; 
+char path[] = "";  
+String message = "";
+String json;
+
+DynamicJsonDocument doc(2048);
+int port = 80;
+
+WiFiClient client;
+
+HttpClient httpClient = HttpClient(client, server, port);
+
+void HTTPpost(){
+
+    httpClient.beginRequest();
+    httpClient.post(path);
+    httpClient.sendHeader("Content-Type", "application/json");
+    httpClient.sendHeader("Content-Length", message.length());
+    httpClient.beginBody();
+    httpClient.print(json);
+    httpClient.endRequest();
+    client.flush(); 
+    
+  }
 
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(9, OUTPUT);
+   Serial.begin(9600);
+   wifiInit();
   
-    LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, dummy, CHANGE);
 }
 
+void loop() {
+  if (client.connect(server, port)) {
 
-void loop() { 
+  HTTPpost();
+  int statusCode = httpClient.responseStatusCode();
+  String response = httpClient.responseBody();
 
-
-     Serial.begin(9600); 
-   
-   //  USBDevice.detach();
-
-    delay(10);
-    Serial.println("vaknade precis loop");
-    Serial.flush();
-    delay(500);
-  digitalWrite(9, HIGH);
-  delay(500);
-  digitalWrite(9, LOW);
-  delay(500);
-            
-  LowPower.sleep(8000);
-    
-//USBDevice.attach();
- delay(1000); 
- 
-}
-void dummy() {
-  
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+  } 
+  else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+   delay(3000); // Wait for 3 seconds to post again
 }
