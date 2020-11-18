d0cb4af3629315014501b4f15ac16f5b86befd60
Simon Olofsson <dotchetter@protonmail.ch>
2020-11-18 04:55:25 +0100

    The Rest api is added as a submodule. Removed legacy files.

--- restapi/woobs/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import DeviceReading
-
-
-class DeviceReadingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DeviceReading
-        fields = ('id', 'date_time', 'temp', 'ph', 'device_id')
--- restapi/woobs/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import DeviceReading
-from .serializers import DeviceReadingSerializer
-
-
-class DeviceReadingView(viewsets.ModelViewSet):
-    queryset = DeviceReading.objects.all()
-    serializer_class = DeviceReadingSerializer
