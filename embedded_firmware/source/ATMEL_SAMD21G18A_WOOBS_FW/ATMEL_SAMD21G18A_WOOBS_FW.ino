f6c177c12b3109cb788dd43e43ee0bfc0381d440
Simon Olofsson <dotchetter@protonmail.ch>
2020-11-20 00:36:31 +0100

    Completed a working version using sensor drivers and wifi connectivity to the W.O.O.B.S api

--- embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/ATMEL_SAMD21G18A_WOOBS_FW.ino
+++ embedded_firmware/source/ATMEL_SAMD21G18A_WOOBS_FW/ATMEL_SAMD21G18A_WOOBS_FW.ino
@@ -1,66 +1,80 @@
+/*
+    W.O.O.B.S main firmware file
+
+    Hardware configuration:
+        MCU:                    Arduino MKR 1010 WiFi (SAMD21)
+        PH SENSOR:              GROVE E201C-BLUE    (Connected to D8)
+        TEMP SENSOR:            DSB18B20 ONEWIRE    (Connected to A1)
+*/
 
 #include <ArduinoHttpClient.h>
-#include "config.h"
-#include "wifi.h"
-
+#include <SPI.h>
+#include <WiFiNINA.h>
 #include "DS18B20.h"
 #include "E201CBlue.h"
 #include "SAMD21_ADC.h"
 #include "definitions.h"
-#include "JsonConverter.h"
 #include <StateMachine.h>
-
-char JsonMessage[200];
-char server[] = ""; 
-char path[] = ""; 
-int port = 80;
-
+#include <ArduinoJson.h>
 
 
 // Heap allocated globally accessible instances (Singletons)
 E201C *phsensor = new E201C(REF_VOLTAGE, ADC_MAX_VAL_REF, PH_REF_1, PH_REF_2, PH_SENSOR_CHANNEL);
-DS18B20 *tempSensor = new DS18B20(0, PORT_PA16); // (Port group, port mask)
-WiFiClient *wifiClient = new WiFiClient();
-HttpClient *httpClient = new HttpClient(client, server, port);
+DS18B20 *tempSensor = new DS18B20(TEMP_PORT_GROUP, TEMP_PINMASK);
+WiFiSSLClient *wifi = new WiFiSSLClient();
+HttpClient *client = new HttpClient(*wifi, API_URL, TSL_PORT);
+
+int status = WL_IDLE_STATUS;
+String response;
+int statusCode = 0;
 
 
-void HTTPpost()
+void SendTelemetryData() 
 {
-    httpClient.beginRequest();
-    httpClient.post(path);
-    httpClient.sendHeader("Content-Type", "application/json");
-    httpClient.sendHeader("Content-Length", message.length());
-    httpClient.beginBody();
-    httpClient.print(json);
-    httpClient.endRequest();
-    client.flush();
+    DynamicJsonDocument doc(1024);
+    doc["ph"] = phsensor->GetSensorPH();
+    doc["temp"] = tempSensor->GetTemperature('C');
+    doc["device_id"] = "DEVICE_ID";
+    
+    char JSONmessageBuffer[300];
+
+    serializeJson(doc, JSONmessageBuffer);
+
+    while (status != WL_CONNECTED) 
+    {
+        status = WiFi.begin(SSID, PASS);
+    }
+
+    Serial.println("making POST request");
+    Serial.println(JSONmessageBuffer);
+
+    client->post("/reading/", "application/json", JSONmessageBuffer);
+
+    statusCode = client->responseStatusCode();
+    response = client->responseBody();
+
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    Serial.print("Response: ");
+    Serial.println(response);
+
 }
 
 
 void setup() 
 {
-    init_adc();
-    init_wifi();
     Serial.begin(115200);
+    init_adc();
 }
-
 
 
 void loop() 
 {
-    Serial.print("PH: ");
-    Serial.println(phsensor->GetSensorPH());
+    static uint64_t last_iter;
 
-    Serial.print("Voltage: ");
-    Serial.println(phsensor->GetSensorVoltage());
-
-    Serial.print(tempSensor->GetTemperature('C'));
-    Serial.print(" C\r\n");
-
- 	ConvertToJson(JsonMessage, tempSensor->GetTemperature('C'), phsensor->GetSensorPH());
-
-	Serial.print(JsonMessage);
-	Serial.println();
-
-    delay(1000);
+    if (millis() - last_iter > STDBY_TIME_MS)
+    {
+        SendTelemetryData();
+        last_iter = millis();
+    }
 }
